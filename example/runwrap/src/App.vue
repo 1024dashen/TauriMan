8fffdab3c0603bbfdd08fc16f2cc9436fba329c7
1024小神 <15670339118@163.com>
2025-05-23 23:06:06 +0800

    Update dependencies in package.json and pnpm-lock.yaml to latest versions, including new Tauri plugins for clipboard management, logging, notifications, and process handling. Enhance App.vue with additional imports for new plugins and update Cargo.toml to reflect these changes. Modify default.json to include new permissions for clipboard and notification functionalities.

--- example/runwrap/src/App.vue
+++ example/runwrap/src/App.vue
@@ -11,6 +11,28 @@ import {
 } from '@tauri-apps/api/app'
 import { enable, isEnabled, disable } from '@tauri-apps/plugin-autostart'
 import { getMatches } from '@tauri-apps/plugin-cli'
+import { writeText, readText } from '@tauri-apps/plugin-clipboard-manager'
+import { register } from '@tauri-apps/plugin-global-shortcut'
+import {
+    warn,
+    debug,
+    trace,
+    info,
+    error,
+    attachConsole,
+    attachLogger,
+} from '@tauri-apps/plugin-log'
+import {
+    isPermissionGranted,
+    requestPermission,
+    sendNotification,
+} from '@tauri-apps/plugin-notification'
+import { openPath } from '@tauri-apps/plugin-opener'
+import { platform } from '@tauri-apps/plugin-os'
+import { exit, relaunch } from '@tauri-apps/plugin-process'
+import Database from '@tauri-apps/plugin-sql'
+import { load } from '@tauri-apps/plugin-store'
+import WebSocket from '@tauri-apps/plugin-websocket'
 import { invoke } from '@tauri-apps/api/core'
 import { Command } from '@tauri-apps/plugin-shell'
 import { open } from '@tauri-apps/plugin-dialog'
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 mod tools;
 use std::sync::{Arc, Mutex};
+use tauri_plugin_log::{Target, TargetKind};
 use tools::model::ServerState;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -13,6 +14,18 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_persisted_scope::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_upload::init())
+        .plugin(tauri_plugin_websocket::init())
         .invoke_handler(tauri::generate_handler![
             tools::comds::get_exe_dir,
             tools::comds::get_man,
@@ -25,6 +38,23 @@ pub fn run() {
                 tauri_plugin_autostart::MacosLauncher::LaunchAgent,
                 Some(vec!["--flag1", "--flag2"]),
             ));
+            #[cfg(desktop)]
+            let _ = app
+                .handle()
+                .plugin(tauri_plugin_updater::Builder::new().build());
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_window_state::Builder::default().build());
+            #[cfg(desktop)]
+            let _ = app
+                .handle()
+                .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}));
+            #[cfg(desktop)]
+            let _ = app.handle().plugin(tauri_plugin_positioner::init());
+            #[cfg(desktop)]
+            let _ = app
+                .handle()
+                .plugin(tauri_plugin_global_shortcut::Builder::new().build());
             #[cfg(desktop)]
             let _ = app.handle().plugin(tauri_plugin_cli::init());
             tauri::async_runtime::block_on(async move {
